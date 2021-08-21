ff978ded24a13162b08fd2f0d4bf8b74f8fe237b
Sven Aas <saas@mtholyoke.edu>
2021-08-21 15:15:26 -0400

    Indentation, trailing space, and newline at EOF.

--- lib/badsec.rb
+++ lib/badsec.rb
@@ -5,60 +5,60 @@ class API_Error < StandardError
 end
 
 class BADSEC_API_Client
-	def initialize(server_uri = "http://localhost:8888")		
-		uri = URI server_uri
-		@service = Net::HTTP.new uri.host, uri.port
+  def initialize(server_uri = "http://localhost:8888")
+    uri = URI server_uri
+    @service = Net::HTTP.new uri.host, uri.port
 
-		# Net::HTTP retries idempotent requests by default. 
-		# In this case we need to use custom retry logic, so this behavior must be disabled.
-		@service.max_retries = 0 
-	end
+    # Net::HTTP retries idempotent requests by default.
+    # In this case we need to use custom retry logic, so this behavior must be disabled.
+    @service.max_retries = 0
+  end
 
-	def get_authentication_token
-		tries = 0
-		begin
-			tries += 1
-			response = @service.head '/auth'
+  def get_authentication_token
+    tries = 0
+    begin
+      tries += 1
+      response = @service.head '/auth'
 
-			# Net::HTTPResponse#value raises Net::HTTPFatalError unless the response code is 200
-			# This is convenient because the error can be rescued and the block retried
-			response.value 
+      # Net::HTTPResponse#value raises Net::HTTPFatalError unless the response code is 200
+      # This is convenient because the error can be rescued and the block retried
+      response.value
 
-			return token = response['Badsec-Authentication-Token'] 
-		rescue Net::HTTPFatalError
-			retry if tries < 3
-			raise API_Error.new("Server returned unsuccessful response code")
-		rescue Net::OpenTimeout 
-			retry if tries < 3
-			raise API_Error.new("Server timed out")
-		rescue StandardError => e
-			retry if tries < 3
-			raise API_Error.new("Server error: #{e}")
-		end
-	end
+      return token = response['Badsec-Authentication-Token']
+    rescue Net::HTTPFatalError
+      retry if tries < 3
+      raise API_Error.new("Server returned unsuccessful response code")
+    rescue Net::OpenTimeout
+      retry if tries < 3
+      raise API_Error.new("Server timed out")
+    rescue StandardError => e
+      retry if tries < 3
+      raise API_Error.new("Server error: #{e}")
+    end
+  end
 
-	def get_noclist
-		token = get_authentication_token
-		checksum = Digest::SHA256.hexdigest(token + '/users')
-		tries = 0		
-		begin
-			tries += 1
-			response = @service.get '/users', initheader = {'X-Request-Checksum' => checksum }
+  def get_noclist
+    token = get_authentication_token
+    checksum = Digest::SHA256.hexdigest(token + '/users')
+    tries = 0
+    begin
+      tries += 1
+      response = @service.get '/users', initheader = {'X-Request-Checksum' => checksum }
 
-			# Net::HTTPResponse#value raises Net::HTTPFatalError unless the response code is 200
-			# This is convenient because the error can be rescued and the block retried
-			response.value 
+      # Net::HTTPResponse#value raises Net::HTTPFatalError unless the response code is 200
+      # This is convenient because the error can be rescued and the block retried
+      response.value
 
-			return response.body.split
-		rescue Net::HTTPFatalError
-			retry if tries < 3
-			raise API_Error.new("Server returned unsuccessful response code")
-		rescue Net::OpenTimeout 
-			retry if tries < 3			
-			raise API_Error.new("Server timed out")
-		rescue StandardError => e
-			retry if tries < 3		
-			raise API_Error.new("Server error: #{e}")
-		end
-	end
+      return response.body.split
+    rescue Net::HTTPFatalError
+      retry if tries < 3
+      raise API_Error.new("Server returned unsuccessful response code")
+    rescue Net::OpenTimeout
+      retry if tries < 3
+      raise API_Error.new("Server timed out")
+    rescue StandardError => e
+      retry if tries < 3
+      raise API_Error.new("Server error: #{e}")
+    end
+  end
 end
--- noclist.rb
+++ noclist.rb
@@ -3,25 +3,25 @@ require_relative './lib/badsec.rb'
 require 'json'
 
 class Noclist
-	def self::print_noclist
-		noclist = []
+  def self::print_noclist
+    noclist = []
 
-		begin
-			badsec = BADSEC_API_Client.new
-			noclist = badsec.get_noclist
-			puts noclist.to_json  
-			return true
-		rescue API_Error => e
-			$stderr.puts e
-			return false
-		end
-	end
+    begin
+      badsec = BADSEC_API_Client.new
+      noclist = badsec.get_noclist
+      puts noclist.to_json
+      return true
+    rescue API_Error => e
+      $stderr.puts e
+      return false
+    end
+  end
 end
 
 if $0 == __FILE__
-	if Noclist.print_noclist
-		exit 0
-	else
-		exit 1
-	end
+  if Noclist.print_noclist
+    exit 0
+  else
+    exit 1
+  end
 end
